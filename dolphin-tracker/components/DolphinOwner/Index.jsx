dd37ed21e4554391263b83cc2bd2619686934afd
jrainey95 <jonathan.rainey5@gmail.com>
2023-10-05 22:34:46 -0700

    days working but not amount of horses per day

--- dolphin-tracker/components/DolphinOwner/Index.jsx
+++ dolphin-tracker/components/DolphinOwner/Index.jsx
@@ -4,14 +4,17 @@ import "./index.scss";
 
 function DolphinOwner() {
   const [data, setData] = useState("");
+  const [raceDays, setRaceDays] = useState([]);
   const [raceTimes, setRaceTimes] = useState([]);
   const [horseNames, setHorseNames] = useState([]);
+  const [currentDay, setCurrentDay] = useState(""); 
 
   useEffect(() => {
     fetch("http://localhost:3001/api/fetchData")
       .then((response) => response.text())
       .then((html) => {
         setData(html);
+        extractRaceDays(html);
         extractRaceTimes(html);
         extractHorseNames(html);
       })
@@ -19,6 +22,20 @@ function DolphinOwner() {
         console.error("Error fetching data:", error);
       });
   }, []);
+
+  // Function to extract race days from HTML
+  const extractRaceDays = (html) => {
+    const dayRegex = /<span class="header__text">([^<]+)<\/span>/g;
+    const matches = html.match(dayRegex);
+
+    if (matches) {
+      const days = matches.map((match) => {
+        return match.match(/<span class="header__text">([^<]+)<\/span>/)[1];
+      });
+      setRaceDays(days);
+      setCurrentDay(days[0]);
+    }
+  };
 
   // Function to extract race times from HTML
   const extractRaceTimes = (html) => {
@@ -32,9 +49,12 @@ function DolphinOwner() {
         raceTime.setHours(raceTime.getHours() - 8); // Convert to PST (UTC -8)
         return raceTime;
       });
+      console.log(extractRaceTimes);
       setRaceTimes(raceTimesPST);
     }
   };
+
+  // Function to extract horse names from HTML
   const extractHorseNames = (html) => {
     const horseNameRegex =
       /<div class="pure-u-2-3 pure-u-sm-1 horse-name"><a[^>]*>([^<]+)<\/a><\/div>/g;
@@ -63,33 +83,9 @@ function DolphinOwner() {
     return `${hoursUntilRace}h ${minutesUntilRace}m`;
   };
 
-  // ...
-
-  // Function to extract the day from a given date string
-  
-  // Function to extract the date from HTML
-  const extractDate = (html) => {
-    const dateRegex = /<span class="header__text">([^<]+)<\/span>/;
-    const match = html.match(dateRegex);
-    
-    if (match) {
-      const extractedDate = match[1]; // Extracted date
-      const dayOfWeek = extractDay(extractedDate); // Extracted day of the week
-      return `${dayOfWeek}, ${extractedDate}`;
-    }
-    
-    return ""; // Default value if not found
-  };
-  
-  const extractDay = (dateString) => {
-    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const dateObj = new Date(dateString);
-    const dayOfWeek = daysOfWeek[dateObj.getDay()];
-    return dayOfWeek;
-  };
-  // ...
-
   return (
+    
+
     <div className="container">
       <div className="scrollable-content">
         <div className="dolphin-content"></div>
@@ -97,48 +93,80 @@ function DolphinOwner() {
       </div>
 
       <div className="time-content">
-        <div className="time-container">
-          <header className="track">{extractDate(data)}</header>
-          
-        </div>
+        {raceDays.map((raceDay, index) => (
+          <div className="race__days ui-accordion open" key={index}>
+            <div className="ui-accordion__header">
+              <span className="header__text">{raceDay}</span>
+            </div>
+            <div className="ui-accordion__content">
+              <table>
+                <thead>
+                  <tr>
+                    <th className="horse">Horse</th>
+                    <th className="time">PST Time</th>
+                    <th className="MTP">Time Until Race</th>
+                    <th className="alert">Alert</th>
+                    <th className="save">Save Horse</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {raceTimes.map((raceTime, index) => (
+                    <tr key={index}>
+                      <td className="horse">{horseNames[index]}</td>
+                      <td>
+                        {raceTime.toLocaleTimeString("en-US", {
+                          timeZone: "America/Los_Angeles",
+                        })}
+                      </td>
+                      <td>{getTimeUntilRace(raceTime)}</td>
+                      <td>
+                        <button className="button-alert">ALERT</button>
+                        <button className="button-alert-all">ALERT ALL</button>
+                      </td>
+                      <td>
+                        <button className="button-save">SAVE</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+
+  );
+}
+
+export default DolphinOwner;
+{/* <div className="block block-system first last odd" id="block- sytem-main">
+  <div id="race-days-wrapper">
+    <div className="race__days ui-accordion open">
+      <div className="ui-accordion__header">
+        <span class="header__text"></span>
+      </div>
+      <div className="ui-accordion__content">
         <table>
           <thead>
             <tr>
               <th className="horse">Horse</th>
-              <th>PST Time</th>
-              <th>Time Until Race</th>
-              <th>Alert</th>
-              <th>Save Horse</th>
+              <th className="time">PST Time</th>
+              <th className="MTP">Time Until Race</th>
+              <th className="alert">Alert</th>
+              <th className="save">Save Horse</th>
             </tr>
           </thead>
           <tbody>
-            {raceTimes.map((raceTime, index) => (
-              <tr key={index}>
-                <td className="horse">{horseNames[index]}</td>
-                <td>
-                  {raceTime.toLocaleTimeString("en-US", {
-                    timeZone: "America/Los_Angeles",
-                  })}
-                </td>
-                <td>{getTimeUntilRace(raceTime)}</td>
-                <td>
-                  <button className="button-alert">ALERT</button>
-                  <button className="button-alert-all">ALERT ALL</button>
-                </td>
-                <td>
-                  <button className="button-save">SAVE</button>
-                </td>
-              </tr>
-        //       <div className="time-container">
-        //   {/* <header className="track">{extractDate(data)}</header> */}
-        // </div>
-            ))}
+            <td className="horse">Horse</td>
+            <td className="time">PST Time</td>
+            <td className="MTP">Time Until Race</td>
+            <td className="alert">Alert</td>
+            <td className="save">Save Horse</td>
           </tbody>
         </table>
       </div>
     </div>
-  );
-  // ...
-}
-
-export default DolphinOwner;
+  </div>
+</div>; */}
